fda8c301e169132dd53cd507655a3f742aa5c002
zpran <zanna@prane.org>
2023-09-19 20:01:41 +0100

    refactor: function fetchData

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,37 @@ import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [people, setPeople] = useState([]);
-  useEffect(() => {
-    axios.get('https://swapi.dev/api/people')
-      .then((data) => {
-        console.log(data);
-        setPeople(data.data?.results);
-        console.log(people);
-        //debugger
-      })
-  }, []);
+	const [people, setPeople] = useState([]);
+	
+	async function fetchData() {
+		const response = await axios.get(
+			"https://swapi.dev/api/people"
+		);
+		setPeople(response.data?.results);
+	}
 
-  return (
-    <>
-      <div className="card">
-        {people.length ?
-          (<>
-            <h4>{people[0].name}</h4>
-            <ul>
-              <li>Hair color: {people[0].hair_color}</li>
-              <li>Eye color: {people[0].eye_color}</li>
-              <li>Birth year: { people[0].birth_year }</li>
-            </ul>
-          </>
-          )
-          : (<>Loading...</>)
-        }
-      </div>
-    </>
-  )
+	useEffect(() => {
+		fetchData();
+	}, []); 
+
+	return (
+		<>
+			<div className="card">
+				{people.length ?
+					(<>
+						<h4>{people[0].name}</h4>
+						<ul>
+							<li>Hair color: {people[0].hair_color}</li>
+							<li>Eye color: {people[0].eye_color}</li>
+							<li>Birth year: { people[0].birth_year }</li>
+						</ul>
+					</>
+					)
+					: (<>Loading...</>)
+				}
+			</div>
+		</>
+	)
 }
 
 export default App
